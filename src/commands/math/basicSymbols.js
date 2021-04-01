35267ffbb6f6841d1ca247df3edadea7cce4667d
Stephan van den Berkmortel <stephan.van.den.berkmortel@algebrakit.nl>
2021-04-01 10:53:22 +0200

    Added "permil" symbol

--- src/commands/math/basicSymbols.js
+++ src/commands/math/basicSymbols.js
@@ -307,6 +307,7 @@ var NonSymbolaSymbol = P(Symbol, function(_, super_) {
 LatexCmds['@'] = NonSymbolaSymbol;
 LatexCmds['&'] = bind(NonSymbolaSymbol, '\\&', '&amp;');
 LatexCmds['%'] = bind(NonSymbolaSymbol, '\\%', '%');
+LatexCmds['permil'] = bind(NonSymbolaSymbol, '\\permil', '&#8240;');
 
 //the following are all Greek to me, but this helped a lot: http://www.ams.org/STIX/ion/stixsig03.html
 
